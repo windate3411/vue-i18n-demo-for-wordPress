b4125064237b28eb634dfe651df3d6029e252eec
windate3411 <windate3411@gmail.com>
2020-09-01 22:20:54 +0800

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ $t("message", { msg: "World!" }) }}</h1>
+    <h2>My name is {{ $t("name") }}</h2>
+    <h2>And my hobbiees are {{ $t("hobby") }}</h2>
+    <h2>I have {{ $tc("brothers", 0) }}</h2>
+    <h2>I have {{ $tc("brothers", 1) }}</h2>
+    <h2>I have {{ $tc("brothers", 2, { count: 2 }) }}</h2>
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`lang${i}`" :value="lang">{{
+        lang
+      }}</option>
+    </select>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      langs: ["tw", "en"],
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
